ec5262286dc4885a3529df7357b4ced51c40ec5f
Adrian <Adrian.Essig@outlook.com>
2024-02-28 16:32:50 +0100

    ls working in loop

--- src/ftp_client.rs
+++ src/ftp_client.rs
@@ -15,38 +15,55 @@ impl FtpClient {
         let mut response = String::new();
 
         read_response(&mut control_stream, &mut response)?;
+        #[cfg(debug_assertions)]
         println!("Server response: {}", response);
 
         send_command(&mut control_stream, &format!("USER {}\r\n", username))?;
         read_response(&mut control_stream, &mut response)?;
+        #[cfg(debug_assertions)]
         println!("Server response: {}", response);
 
         send_command(&mut control_stream, &format!("PASS {}\r\n", password))?;
         read_response(&mut control_stream, &mut response)?;
+        #[cfg(debug_assertions)]
         println!("Server response: {}", response);
-
+        #[cfg(debug_assertions)]
         println!("Connected to {}", addr);
 
         Ok(Self { control_stream })
     }
 
     pub (crate) fn list(&mut self) -> std::io::Result<()> {
+        println!("LIST COMMAND:");
         let mut response = String::new();
 
         send_command(&mut self.control_stream, "EPSV\r\n")?; //using epsv instead of pasv because it's newer and allows for ipv6
         read_response(&mut self.control_stream, &mut response)?;
+        #[cfg(debug_assertions)]
         println!("Server response: {}", response);
         let port = parse_port(&response);
 
+        #[cfg(debug_assertions)]
         println!("Connecting to data port: {}", port);
         let mut data_stream = TcpStream::connect(format!("localhost:{}", port))?;
 
-
+        #[cfg(debug_assertions)]
         println!("Sending LIST command");
         send_command(&mut self.control_stream, "LIST\r\n")?;
 
+        read_response(&mut self.control_stream, &mut response)?;
+        #[cfg(debug_assertions)]
+        println!("Server response: {}", response);
+
         read_response(&mut data_stream, &mut response)?;
-        println!("Server response:\n{}", response);
+        println!("{}", response);
+
+        read_response(&mut self.control_stream, &mut response)?;
+        #[cfg(debug_assertions)]
+        println!("Server response: {}", response);
+
+
+        data_stream.shutdown(std::net::Shutdown::Both)?;
         Ok(())
     }
 }
@@ -70,6 +87,7 @@ fn parse_port(response : &str) -> String{
     let mut parts: Vec<&str> = response_without_linebreaks.split("|||").collect();
 
     let parts = parts[1].split("|").collect::<Vec<&str>>();
+    #[cfg(debug_assertions)]
     println!("{:?}", parts[0]);
     return parts[0].to_string();
 }
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,41 @@ mod command_parser;
 mod ftp_client;
 
 use std::io::stdin;
+use crate::command_parser::Command;
 
 fn main() {
     let ftp_address = "localhost:21";
     let username = "user";
     let password = "pass";
 
-    let mut client = ftp_client::FtpClient::new(ftp_address, username, password);
+    if let Ok(mut client) = ftp_client::FtpClient::new(ftp_address, username, password) {
+        println!("Connected to ftp server");
+
+        loop {
+            let mut input = String::new();
+            let b1 = stdin().read_line(&mut input);
+
+            let command = command_parser::determine_command(input);
+            println!("{:?}", command);
+
+            match command {
+                Command::LS => {client.list();}
+                Command::GET => {}
+                Command::MGET => {}
+                Command::NONE => {println!("Invalid command");}
+            }
+        }
+    } else {
+        println!("Failed to connect to ftp server");
+    }
 
 
-    let mut input = String::new();
-    let b1 = stdin().read_line(&mut input);
 
-    let command = command_parser::determine_command(input);
-    println!("{:?}", command);
+    // let mut input = String::new();
+    // let b1 = stdin().read_line(&mut input);
+    //
+    // let command = command_parser::determine_command(input);
+    // println!("{:?}", command);
 
 
     // if command == command_parser::Command::LS {
